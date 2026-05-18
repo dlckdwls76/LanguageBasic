697d64c1306223962215f9e5a6d7eef7f5ef528c
L_CJ76 <dlckdwls76@gmail.com>
2026-05-18 23:33:37 +0900

    2026/05/18

---
+++ CodeTest1/Factorial/Factorial.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int a=1;
+    int sum=1;
+    
+    while(1){
+        sum*=a;
+        if(sum>n) break;
+        a++;
+    }
+    
+    a--;
+    
+ 
+
+    return a;
+}
+int main()
+{
+    int n = 24;
+    
+    cout << solution(n) << endl;
+    return 0;
+    
+}
